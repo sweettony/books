8ed60eef0fb1ee8c7a59daa1f0b42ffafe8cc3fd
tony ye <tanghan.ye@qq.com>
2021-01-13 16:48:14 +0800

    add CRC 算法

---
+++ CRC/crc.h
@@ -0,0 +1,15 @@
+#ifndef _CRC_H_
+
+#ifndef uint8
+#define uint8  unsigned char
+#else
+#warning ("warning: define uint8 in other file from file crc.h")
+#warning 
+
+#endif
+#ifndef uint32
+#define uint32 unsigned int
+#else
+#warning (warning: "define uint32 in other file from file crc.h")
+#endif
+#endif //_CRC_H_
